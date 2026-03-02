7479a995e83265bf3e9430f4f6996664bfbb9615
lskld <theo.lillieskold@gmail.com>
2026-03-02 10:07:44 +0100

    setup base page structure and add routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Projects from './pages/Projects'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cv">CV</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cv" element={<Cv />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Cv.jsx
@@ -0,0 +1,9 @@
+function Cv() {
+    return (
+        <div>
+            <h1>CV</h1>
+        </div>
+    );
+}
+
+export default Cv;
