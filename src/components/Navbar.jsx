155b7a7b411d8dd56ab4f32644f21ec2349fc538
lskld <theo.lillieskold@gmail.com>
2026-03-02 15:42:45 +0100

    changed navbar style and colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/cv">CV</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/about">About</Link></li>
+                <li>
+                    <NavLink to="/" end className={({ isActive }) => isActive ? "active" : ""}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/cv" className={({ isActive }) => isActive ? "active" : ""}>
+                        CV
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>
+                        Projects
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>
+                        About
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
