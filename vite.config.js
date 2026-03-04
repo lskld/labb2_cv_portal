4664c4514cc50a75838f58fbfbb9f475ccc21792
lskld <theo.lillieskold@gmail.com>
2026-03-04 16:14:04 +0100

    updated base adress and removed a '/' in the end for refreshing to work

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/labb2_cv_portal/'
+  base: '/labb2_cv_portal'
 })
